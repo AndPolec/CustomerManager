a30ca554fc8b4a730f1edf3b45ebfbeb14ce5970
AndPolec <xardas345@gmail.com>
2024-02-26 21:02:51 +0100

    Deleted DataAccess class

--- CustomerManager.Infrastructure/IDataAccess.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CustomerManager.Infrastructure
-{
-    public interface IDataAccess
-    {
-        Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters);
-        Task SaveData<T>(string storedProcedure, T parameters);
-    }
-}
