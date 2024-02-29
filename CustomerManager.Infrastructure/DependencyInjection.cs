57c4e5147e728d0558a397f84a1f7af35ede743c
AndPolec <xardas345@gmail.com>
2024-02-29 20:40:39 +0100

    Added Infrastructure DI file

---
+++ CustomerManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using CustomerManager.Domain.Interfaces;
+using CustomerManager.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            return services;
+        } 
+    }
+}
---
+++ CustomerManager.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using CustomerManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public CustomerRepository()
+        {
+            
+        }
+    }
+}
