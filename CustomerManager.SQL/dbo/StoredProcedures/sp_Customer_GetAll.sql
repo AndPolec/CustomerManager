fe543a35d9b207c10c140f71a4e4af595c875127
AndPolec <xardas345@gmail.com>
2024-03-01 19:38:59 +0100

    Added sp

---
+++ CustomerManager.SQL/dbo/StoredProcedures/sp_Customer_GetAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[sp_Customer_GetAll]
+AS
+BEGIN
+	select * 
+	from dbo.Customer c
+	join dbo.[Address] a on a.CustomerId = c.Id
+	join dbo.ContactPerson cp on cp.CustomerId = c.Id;
+END
