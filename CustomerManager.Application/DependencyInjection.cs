af2d84adc6f95f3a75f18fafea9c0fd6e0f0c4f4
AndPolec <xardas345@gmail.com>
2024-03-03 17:27:05 +0100

    Added Di for Application

---
+++ CustomerManager.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using CustomerManager.Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<ICustomerService, ICustomerService>();
+        }
+    }
+}
