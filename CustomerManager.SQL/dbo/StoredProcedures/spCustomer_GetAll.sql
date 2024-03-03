cdb5ceff57397148ccc57ce304794dc8691b2f1c
AndPolec <xardas345@gmail.com>
2024-03-03 16:30:27 +0100

    Added pagination to spCustomer_GetAll

---
+++ CustomerManager.SQL/dbo/StoredProcedures/spCustomer_GetAll.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spCustomer_GetAll]
+@CustomerOwnerId int,
+@SearchString nvarchar(200),
+@PageSize int,
+@PageNumber int
+AS
+BEGIN
+	select c.*, a.* 
+	from dbo.Customer c
+	left join dbo.[Address] a on a.CustomerId = c.Id
+	where c.CustomerOwnerId = @CustomerOwnerId 
+		and (@SearchString is null or c.Name like '%' + @SearchString + '%')
+	order by c.Name asc
+	offset (@PageSize-1)*@PageSize rows
+	fetch next @PageSize rows only;
+END
