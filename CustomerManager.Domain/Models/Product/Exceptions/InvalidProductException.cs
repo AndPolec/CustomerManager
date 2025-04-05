5d401a7516bc2ae22ad3e645e748904417d5423c
Andrzej Połeć <xardas345@gmail.com>
2025-04-05 20:45:56 +0200

    feat(domain): add custom exceptions for Product and related objects

---
+++ CustomerManager.Domain/Models/Product/Exceptions/InvalidProductException.cs
@@ -0,0 +1,16 @@
+﻿using CustomerManager.Domain.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Domain.Models.Product.Exceptions
+{
+    public class InvalidProductException: DomainException
+    {
+        public InvalidProductException(string? message) : base(message)
+        {
+        }
+    }
+}
---
+++ CustomerManager.Domain/Models/Product/Exceptions/InvalidProductUnitException.cs
@@ -0,0 +1,16 @@
+﻿using CustomerManager.Domain.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Domain.Models.Product.Exceptions
+{
+    public class InvalidProductUnitException: DomainException
+    {
+        public InvalidProductUnitException(string? message) : base(message)
+        {
+        }
+    }
+}
