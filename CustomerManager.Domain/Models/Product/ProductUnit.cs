b14a21fb4b31f53cf942349a3dc5a1aecbd9876b
Andrzej Połeć <xardas345@gmail.com>
2025-04-07 20:50:38 +0200

    feat(domain): add ProductUnit

--- CustomerManager.Domain/Models/Product/ProductUnit.cs
+++ CustomerManager.Domain/Models/Product/ProductUnit.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CustomerManager.Domain.Models.Dictionaries;
+using CustomerManager.Domain.Models.Product.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,46 @@ using System.Threading.Tasks;
 
 namespace CustomerManager.Domain.Models.Product
 {
-    internal class ProductUnit
+    public class ProductUnit
     {
+        public int Id { get; private set; }
+
+        public int ProductId { get; private set; }
+
+        public int UnitId { get; private set; }
+
+        public decimal ConversionFactor { get; private set; }
+
+        public bool IsDefault { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public string? CreatedBy { get; private set; }
+
+        public ProductUnit(int productId, int unitId, decimal conversionFactor, bool isDefault = false, string? createdBy = null)
+        {
+            if (conversionFactor <= 0)
+                throw new InvalidProductUnitException("Conversion factor must be greater than zero.");
+
+            ProductId = productId;
+            UnitId = unitId;
+            ConversionFactor = conversionFactor;
+            IsDefault = isDefault;
+            CreatedAt = DateTime.UtcNow;
+            CreatedBy = createdBy;
+        }
+
+        public void SetDefault(bool isDefault)
+        {
+            IsDefault = isDefault;
+        }
+
+        public void UpdateConversionFactor(decimal newFactor)
+        {
+            if (newFactor <= 0)
+                throw new InvalidProductUnitException("Conversion factor must be greater than zero.");
+
+            ConversionFactor = newFactor;
+        }
     }
 }
